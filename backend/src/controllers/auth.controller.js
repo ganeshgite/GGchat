3f574b69fc96b771724e89e87b224220526d1fb5
ganeshgite <giteganesh09@gmail.com>
2026-05-28 16:25:20 +0530

    login logout functionality added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -76,3 +76,39 @@ const user = await User.findOne({ email })
         res.status(500).json({message:"internal server error"})
     }
 }
+
+export const login = async (req,res)=>{
+    const {email,password} = req.body;
+console.log(res)
+    try{
+        const user = await User.findOne({email});
+        if(!user) return res.status(400).json({message:"Invalid Credentials"})
+
+        const isPasswordCorrect = await bcrypt.compare(password,user.password)
+        if(!isPasswordCorrect) return res.status(400).json({message:"Invalid Credentials"})
+
+        generateToken(user._id,res)
+
+        res.status(200).json({
+             _id:user._id,
+                fullName:user.fullName,
+                email:user.email,
+                profilePic:user.profilePic
+
+        })
+
+    }
+      catch(err)
+    {
+        console.log("Error In login controller ", err)
+        res.status(500).json({message:"Internal Error"})
+        
+    }
+}
+
+export const logout = (req,res)=>{
+    const token = res.cookie("token","");
+
+    res.status(200).json({message:" Logout Successfully"})
+
+}
